3012d64d55b1a45ff05388c162860d7d12e58fee
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-09-04 10:57:45 +0200

    add vector argument in smooothing

--- src/robustnonparamregression.jl
+++ src/robustnonparamregression.jl
@@ -1,4 +1,4 @@
-function r̂ₙ(x, X, Y, kernel)
+function r̂ₙ(x, X::AbstractArray, Y::AbstractArray, kernel::SmoothingKernel)
     Nₕ = [kernel.f((X[i] - x) / kernel.h) for i in eachindex(X)]
     sumNₕ = sum(Nₕ)
     if sumNₕ == 0
@@ -6,6 +6,21 @@ function r̂ₙ(x, X, Y, kernel)
     else
         return sum(Nₕ[i] * Y[i] for i in eachindex(Y)) / sum(Nₕ)
     end
+end
+
+function RobustMeans.r̂ₙ(x::AbstractArray, X::AbstractArray, Y::AbstractArray, kernel::SmoothingKernel)
+	out = zeros(eltype(Y), length(x))
+	Nₕ = similar(Y)
+	for (j, xx) in enumerate(x)
+    	Nₕ[:] = [kernel.f((X[i] - xx) / kernel.h) for i in eachindex(X)]
+    	sumNₕ = sum(Nₕ)
+    	if sumNₕ ≈ 0
+        	continue
+    	else
+        	out[j] = sum(Nₕ[i] * Y[i] for i in eachindex(Y)) / sum(Nₕ)
+    	end
+	end
+	return out
 end
 
 function r̂ₙMOM(x, X, Y, kernel, m)
